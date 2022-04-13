9667280fc77afcdedefb051e5ab2ccba26fe2ae0
user <sayidbek-bakhrom>
2022-04-13 08:36:15 +0500

    add store app

--- store/views.py
+++ store/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Category, Product
 
-# Create your views here.
+
+def all_products(request):
+    products = Product.objects.all()
+    context = {
+        'products': products
+    }
+    return render(request, 'store/home.html', context)
